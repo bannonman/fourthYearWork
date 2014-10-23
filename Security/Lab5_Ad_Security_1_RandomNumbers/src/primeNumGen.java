fc99348d0103e69793548d6631660c6306988eb1
corey bannon <corey.bannon@mydit.ie>
2014-10-23 16:44:40 +0100

    Changing structure

--- Security/Lab5_Ad_Security_1_RandomNumbers/src/primeNumGen.java
+++ Security/Lab5_Ad_Security_1_RandomNumbers/src/primeNumGen.java
@@ -100,7 +100,8 @@ public class primeNumGen
 		    //check if n is a multiple of 2
 		    if (n%2==0) return false;
 		    //if not, then just check the odds
-		    for(int i=3;i*i<=n;i+=2) {
+		    for(int i=3;i*i<=n;i+=2) 
+                    {
 		        if(n%i==0)
 		            return false;
 		    }
